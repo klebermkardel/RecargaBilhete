1ee78ec5e48c4261a29f234a07734a2b8cb3b9f2
Kleber Molina Kardel <molinakardel@gmail.com>
2023-08-26 08:20:05 -0300

    :sparkles: feat: Define layout com label e textbox para usuário inserir o número do seu bilhete

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -29,22 +29,45 @@
         private void InitializeComponent()
         {
             this.LbTitle = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
             // LbTitle
             // 
             this.LbTitle.AutoSize = true;
             this.LbTitle.Font = new System.Drawing.Font("Arial", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.LbTitle.Location = new System.Drawing.Point(180, 45);
+            this.LbTitle.Location = new System.Drawing.Point(171, 45);
             this.LbTitle.Name = "LbTitle";
-            this.LbTitle.Size = new System.Drawing.Size(0, 33);
+            this.LbTitle.Size = new System.Drawing.Size(594, 33);
             this.LbTitle.TabIndex = 0;
+            this.LbTitle.Text = "Seja bem vindo ao Sistema RecargaBilhete";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(319, 102);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(281, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Insira o número do seu bilhete:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Arial", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(343, 140);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(221, 30);
+            this.textBox1.TabIndex = 2;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(932, 580);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.LbTitle);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -56,6 +79,8 @@
         #endregion
 
         private System.Windows.Forms.Label LbTitle;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
     }
 }
 
